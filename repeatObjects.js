23ce40c8a2658bfa3d2ba080fae003546a51272d
Iakov <kirbabin.jacob@gmail.com>
2025-02-02 13:58:16 +0300

    fast

---
+++ repeatObjects.js
@@ -0,0 +1,38 @@
+// 1) Получили объект, вернули сумму всех числовых полей
+
+const someData = {
+	notNum: 'Vasya',
+	one: 1,
+	four: 4,
+	two: 2,
+    three: 3,
+    five: 5,
+};
+
+function returnSum(obj) {
+	let counter = 0;
+	for (let key in obj) {
+		typeof obj[key] === 'number' ? (counter += obj[key]) : counter;
+	}
+	return counter;
+}
+
+console.log(returnSum(someData));
+
+// 2) (*) // Перебрали объект - ключи в массив - массив сортируем - перебираем массив - в нем перебираем объект и сравниваем по соответствию
+
+function returnArr(obj) {
+	const arr = [];
+	for (let key in obj) {
+		typeof obj[key] === 'number' ? arr.push(obj[key]) : arr;
+	}
+	return arr.sort((a, b) => b - a).map(el => {
+        for (let key in obj) {
+            if(obj[key] === el){
+                return key;
+            }
+        }
+    })
+}
+
+console.log(returnArr(someData));
